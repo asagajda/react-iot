c16d27885ac14b287a2d1595c31c121fc12b55a6
artem <asagajda@gmail.com>
2017-07-14 21:08:27 +0300

    true async

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ import IconButton from 'material-ui/IconButton';
 import Paper from 'material-ui/Paper';
 //import logo from './logo.svg';
 import './App.css';
-import Web3 from 'web3';
 
 import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
@@ -50,8 +49,8 @@ function sleep(milliseconds) {
   }
 }
 
-
-var ETHEREUM_CLIENT = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
+// var ETHEREUM_CLIENT = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
+var ETHEREUM_CLIENT = window.web3
 
 var dougABI = [{"constant":false,"inputs":[{"name":"name","type":"string"}],"name":"contracts","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"name","type":"string"}],"name":"removeContract","outputs":[{"name":"result","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"remove","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"name","type":"string"},{"name":"addr","type":"address"}],"name":"addContract","outputs":[{"name":"result","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"contracts","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"inputs":[],"payable":false,"type":"constructor"}]
 
@@ -207,44 +206,148 @@ class MyApp extends Component {
 }
 
 class DevicesList extends Component {
-  render(){
-    var es = this.props.es
-    var am = this.props.am
-    console.log("es, am")
-    console.log(es, am)
 
-    if (am === undefined || es === undefined)
+  DEVICE_FIELDS_COUNT = 4;
+
+  constructor(props){
+    super(props)
+    this.state = {
+      devices: {}
+    }
+  }
+
+  componentWillReceiveProps(nextProps){
+    console.log(nextProps)
+    this.componentDidMount(nextProps)
+  }
+
+  componentDidMount(props){
+    console.log("did mount")
+    if (props === undefined || props.am === undefined || props.es === undefined) return;
+    console.log("coo-coo")
+    var es = props.es
+    var am = props.am
+
+    var devices = {}
+    var counter;
+    am.getDevicesCount((e,r) => {
+      if (!e){
+        counter = r.toString() * this.DEVICE_FIELDS_COUNT;
+        var currentIndex;
+        es.getDllIndex.call("0x0",true, (e, r) => {
+          if (!e){
+            currentIndex = r;
+            //-----------async loop-------------
+            this.asyncLoop(counter, (loop) => {
+
+                    // getting next index r2
+                    es.getDllIndex.call(currentIndex, true, (e2,r2) => {
+                      if (!e2){
+                        console.log("must not be 0x0",r2)
+                        // if got next index than going to get current index info
+                        es.getInfoByHash(currentIndex, (e,r) => {
+                          if (!e){
+                            var id = r[2].toString()
+                            console.log("counter", counter)
+                            // if already got entry for id of current index than not fethcing Info
+                            // but going forward to next index (r2)
+                            if (devices[id] === undefined) {
+
+                              am.getDeviceById.call(id, (e1,r1) => {
+                                if (!e1){
+                                  // update devices list
+                                  devices[id] = r1;
+                                  // then go to next index
+                                  currentIndex = r2
+                                  loop.next();
+                                } else {
+                                  // didn't got dexice
+                                  console.log(e1);
+                                  loop.break();
+                                }
+                              })
+                            } else {
+                              // then go to next index, passing devices update
+                              currentIndex = r2
+                              loop.next();
+                            }
+                          } else {
+                            // didn't got info
+                            console.log(e);
+                            loop.break();
+                          }
+                        });
+                      } else {
+                        // didnt got next index
+                        console.log(e2);
+                        loop.break();
+                      }
+                    });
+            },
+            // when all done
+            ()=>{
+              console.log('loop finished')
+              console.log(devices)
+
+              this.setState({devices: devices})
+            }
+          )
+          //--------end asymc loop----------------
+          } else {
+            console.log(e)
+          }
+        });
+      }
+    });
+  }
+
+  asyncLoop(iterations, func, callback) {
+      var index = 0;
+      var done = false;
+      var loop = {
+          next: function() {
+              if (done) {
+                  return;
+              }
+
+              if (index < iterations) {
+                  index++;
+                  func(loop);
+                  console.log(index,iterations)
+              } else {
+                  done = true;
+                  callback();
+              }
+          },
+
+          iteration: function() {
+              return index - 1;
+          },
+
+          break: function() {
+              done = true;
+              callback();
+          }
+      };
+      loop.next();
+      return loop;
+  }
+
+  render(){
+    if (this.props.am === undefined || this.props.es === undefined)
     {return (
       <div>loading devices list...</div>
     )}
 
-    var devices = {}
-    var currentIndex = es.getDllIndex("0x0",true);
-
-    // TODO: make it async way
-    while (parseInt(currentIndex, 16) !== 0)
-    {
-      var index_info = es.getInfoByHash.call(currentIndex)
-      var new_index = es.getDllIndex(currentIndex,true)
-      var id = index_info[2].toString()
-      if (devices[id] === undefined) {
-        var result = am.getDeviceById.call(id)
-        devices[id] = result
-      }
-      currentIndex = new_index
-    }
-
-    var output =
-    <div>
+    return (<div>
       <p>Devices registered on blockchain (iterated through devices full index): </p>
         {
-        Object.keys(devices).map(function(key){
-                    return <Device fields={devices[key]} key={key} id={key}/>;
-                  })
+        Object.keys(this.state.devices).map(function(key){
+                    return <Device fields={this.state.devices[key]} key={key} id={key}/>;
+                  }.bind(this))
         }
-    </div>
+    </div>)
 
-    return output;
   }
 }
 
@@ -336,7 +439,6 @@ class ContractsInfo extends Component {
         </CardText>
       </Card>
     </Paper>
-    console.log("remdered")
     return output;
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Web3 from 'web3';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+window.addEventListener('load', function() {
+
+  // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+  if (typeof web3 !== 'undefined') {
+    // Use Mist/MetaMask's provider
+    window.web3 = new Web3(window.web3.currentProvider);
+  } else {
+    console.log('No web3? You should consider trying MetaMask!')
+    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+    window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+  }
+
+  ReactDOM.render(<App />, document.getElementById('root'));
+
+})
+
+
 registerServiceWorker();
